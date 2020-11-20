62e1ca95f7fb460dc13a5de21bb1d3e6560f5179
DalangLy <dalangly@gmail.com>
2020-11-20 16:59:42 +0700

    - add token to connect to authorize hub and get back connectionid and logged in username

--- SignalR Client/Form1.cs
+++ SignalR Client/Form1.cs
@@ -31,6 +31,10 @@ namespace SignalR_Client
         public async void Connect()
         {
             Connection = new HubConnection(ServerUrl);
+
+            //to access autorized hub and map logged user u need to pass token
+            Connection.Headers.Add("Authorization", String.Format("Bearer {0}", "0hHhYVvKw51eJbritK1Jdqz5_j-ys_53ISauXGQHH5yQSMogFeLcKxKULGDiodVDJeodCW24M44xF-llhK-mDTa0eP0_sJ4LTWxZsk8q0jPbk31wCDITsJeniGzi93gHhlUW7kMasu-ilctf4bvkKqKX0kAstJxJMRsth0Fffm-BGUfvlZKjUNTvtCOzHrU5XuuNl_0y50okcuiY7m6NbtBQis2YsFrNkBH00bTveZjGn_83aOwFi0rhFUQaYRdyll98F72hQr9TJxqBNDQyuMuIU117MqHT8H8J7O2drIaQ0Y0pyc3ju28cMzo-rfg7V233LxhtB-4ta71Z-vCjSvnec_JEY_7SBs7BY23j6wiyupTox_Q_6V3juh4jexFBDSoWNCYw-qNlyAuQFuUbpOO5XGHsIw1Z9reG8ttTDnZILIuVbrRppFXWrLKFmxsBGcsOYL2NUB30QPCiysAzL75bO4aoxWv8y1iZCA2gNAk")); //THIS IS WHERE YOU ADD YOUR ACCESS TOKEN MENTIONED ABOVE
+
             HubProxy = Connection.CreateHubProxy("SignalRTestHub"); //hub name
             AnotherHubProxy = Connection.CreateHubProxy("AnotherSignalRTestHub");
             await Connection.Start();
@@ -46,9 +50,18 @@ namespace SignalR_Client
 
             HubProxy.On("anotherSayHello", (message) =>
                 this.Invoke((Action)(() =>
-                {
-                    Console.WriteLine(Convert.ToString(message));
-                }
+                        {
+                            Console.WriteLine(Convert.ToString(message));
+                        }
+                    )
+                )
+            );
+            
+            HubProxy.On("allConnectedUsers", (message) =>
+                        this.Invoke((Action)(() =>
+                        {
+                            Console.WriteLine(Convert.ToString(message));
+                        }
                     )
                 )
             );
